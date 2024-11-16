b606cdd8d43201e1d0006bad82872c327c58d9f3
aquestry <155507142+aquestry@users.noreply.github.com>
2024-11-16 21:08:50 +0100

    Testing

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -4,62 +4,76 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.*;
-import net.minestom.server.extras.velocity.VelocityProxy;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerCommandEvent;
+import net.minestom.server.event.player.PlayerMoveEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
-import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.item.ItemStack;
-import net.minestom.server.item.Material;
-import net.minestom.server.network.packet.server.common.PluginMessagePacket;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     private static InstanceContainer instanceContainer;
+    private static final Map<Player, Pos> parkourPositions = new HashMap<>();
 
     public static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         instanceContainer = instanceManager.createInstanceContainer();
-        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 1, Block.STONE_BRICKS));
-        String vsecret = System.getenv("PAPER_VELOCITY_SECRET");
-        if (vsecret != null) {
-            VelocityProxy.enable(vsecret);
-            System.out.println("secret: " + vsecret);
-        }
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 1, Block.AIR));
+
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             Player player = event.getPlayer();
             event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0, 1, 0));
+            player.setRespawnPoint(new Pos(0, 60, 0));
+            initializeParkour(player);
         });
-        globalEventHandler.addListener(PlayerSpawnEvent.class, event -> event.getPlayer().getInventory().addItemStack(ItemStack.builder(Material.IRON_AXE).build()));
-        globalEventHandler.addListener(PlayerDeathEvent.class, event -> {
-            event.setChatMessage(null);
-            for (Player p : instanceContainer.getPlayers()) {
-                sendToLobby(p);
+
+        globalEventHandler.addListener(PlayerMoveEvent.class, event -> {
+            Player player = event.getPlayer();
+            Pos position = player.getPosition();
+            if (position.y() < 0) {
+                player.teleport(new Pos(0, 60, 0));
+                initializeParkour(player);
+            }
+            generateNextParkourBlock(player, position);
+        });
+
+        globalEventHandler.addListener(PlayerCommandEvent.class, event -> {
+            Player player = event.getPlayer();
+            if (event.getCommand().equalsIgnoreCase("leave")) {
+                player.kick("You have left the game.");
             }
         });
-        globalEventHandler.addListener(PlayerBlockBreakEvent.class, event -> event.setCancelled(true));
-        globalEventHandler.addListener(PlayerDisconnectEvent.class, event -> {
-            for (Player p : instanceContainer.getPlayers()) {
-                if (!p.equals(event.getPlayer())) {
-                    sendToLobby(p);
-                }
-            }
-        });
-        instanceContainer.setChunkSupplier(LightingChunk::new);
+
         minecraftServer.start("0.0.0.0", 25565);
     }
 
-    public static void sendToLobby(Player player) {
-        String message = "lobby:" + player.getUsername();
-        PluginMessagePacket packet = new PluginMessagePacket(
-                "nebula:main",
-                message.getBytes(StandardCharsets.UTF_8)
-        );
-        player.sendPacket(packet);
+    private static void initializeParkour(Player player) {
+        Pos startBlock = new Pos(0, 59, 0);
+        instanceContainer.setBlock(startBlock.blockX(), startBlock.blockY(), startBlock.blockZ(), Block.STONE);
+        parkourPositions.put(player, startBlock);
+    }
+
+    private static void generateNextParkourBlock(Player player, Pos position) {
+        Pos lastBlock = parkourPositions.get(player);
+        if (lastBlock != null && position.distanceSquared(lastBlock) > 4) {
+            int offsetX = ThreadLocalRandom.current().nextInt(-1, 2);
+            int offsetZ = ThreadLocalRandom.current().nextInt(-1, 2);
+            int offsetY = ThreadLocalRandom.current().nextBoolean() ? 1 : 0;
+
+            int nextX = lastBlock.blockX() + offsetX;
+            int nextY = lastBlock.blockY() + offsetY;
+            int nextZ = lastBlock.blockZ() + offsetZ;
+
+            Pos nextBlock = new Pos(nextX, nextY, nextZ);
+            instanceContainer.setBlock(nextBlock.blockX(), nextBlock.blockY(), nextBlock.blockZ(), Block.STONE);
+            parkourPositions.put(player, nextBlock);
+        }
     }
 }
